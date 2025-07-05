f6db6130e3abc7359902fc8d6a6e352dab66d751
tk59kim <taekyung.kim.1123@gmail.com>
2025-07-06 00:31:16 +0900

    Initial DRSS upload

---
+++ ver_vs_ham.py
@@ -0,0 +1,27 @@
+import fastf1
+from fastf1.plotting import setup_mpl
+from matplotlib import pyplot as plotting
+
+#enable data cache (download race data one and stores it)
+fastf1.Cache.enable_cache('cache')
+
+#Load qualifying session from Monza 2023
+session = fastf1.get_session(2023,'Monza', 'Q')
+
+#Pick fastest laps from VER and HAM
+ver = session.laps.pick_driver('VER').pick_fastest()
+ham = session.laps.pick_driver('HAM').pick_fastest()
+
+#Get telemetry data
+ver_tel = ver.get_car_data().add_distance()
+ham_tel = ham.get_car_data().add_distance()
+
+#Plot speed vs distance
+plt.plot(ver_tel['distance'], ver_tel['Speed'], label='VER')
+plt.plot(ham_tel['distance'], ham_tel['Speed'], label='HAM')
+
+plt.titel("Monza 2023 Qualifying - Fastest Laps")
+plt.xlable("Distance (m)")
+plt.ylabel("Speed (km/h)")
+plt.legand()
+plt.show()
